5ddc9ca2020f3611f7fe16974d60e582abfd42e9
lenovo r9000x <zhangzhenqian1996@gmail.com>
2022-07-11 10:56:25 +0800

    fixed bug

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 // 应用的根组件  
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import Login from './pages/login/login.jsx'
-import Admin from './pages/admin/admin.jsx'
-import Home from './pages/home/index.jsx';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CidGraph from "./pages/cid/index.jsx";
 import { WagmiConfig, createClient } from 'wagmi'
 const client = createClient()
@@ -12,11 +9,7 @@ const App = () => {
     <WagmiConfig client={client}>
       <BrowserRouter>
           <Routes>
-              {/* 默认初始化入口/home*/}
-              <Route path="/*" element={<Admin/>} />
-              {/* home模块路由 */}
-              <Route path="login" element={<Login />} />
-              <Route path="cid" element={<CidGraph />} />
+              <Route path="/*" element={<CidGraph />} />
           </Routes>
       </BrowserRouter>
   </WagmiConfig>
